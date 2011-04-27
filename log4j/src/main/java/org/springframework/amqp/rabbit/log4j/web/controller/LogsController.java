668d8905648e6b760e8700be496b78afe641cfce
Dave Syer <dsyer@vmware.com>
2011-04-27 13:19:42 +0100

    Organize imports

--- log4j/src/main/java/org/springframework/amqp/rabbit/log4j/web/controller/LogsController.java
+++ log4j/src/main/java/org/springframework/amqp/rabbit/log4j/web/controller/LogsController.java
@@ -28,7 +28,6 @@ import org.springframework.amqp.rabbit.log4j.listener.AmqpLogMessageListener;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
