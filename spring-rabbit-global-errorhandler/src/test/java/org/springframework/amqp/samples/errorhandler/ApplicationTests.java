142f2744a0cf7befb6632895a64ca4c1ac1c3e61
Gary Russell <grussell@vmware.com>
2020-09-16 09:39:42 -0400

    Upgrade to 2.3.0-M2

--- spring-rabbit-global-errorhandler/src/test/java/org/springframework/amqp/samples/errorhandler/ApplicationTests.java
+++ spring-rabbit-global-errorhandler/src/test/java/org/springframework/amqp/samples/errorhandler/ApplicationTests.java
@@ -1,11 +1,9 @@
 package org.springframework.amqp.samples.errorhandler;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class ApplicationTests {
 
